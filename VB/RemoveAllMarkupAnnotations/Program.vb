b39732a291f9104716c16f59ab06cb88c14931fe
DevExpressExampleBot <fakeEmail>
2022-02-02 12:29:28 +0300

    Source auto update [skip ci]

--- VB/RemoveAllMarkupAnnotations/Program.vb
+++ VB/RemoveAllMarkupAnnotations/Program.vb
@@ -1,27 +1,23 @@
-﻿Imports DevExpress.Pdf
+Imports DevExpress.Pdf
 Imports System.Collections.Generic
 
 Namespace RemoveAllMarkupAnnotations
 
     Friend Class Program
-        Shared Sub Main(ByVal args() As String)
 
+        Shared Sub Main(ByVal args As String())
             ' Create a PDF Document Processor.
-            Using processor As New PdfDocumentProcessor()
-
+            Using processor As PdfDocumentProcessor = New PdfDocumentProcessor()
                 ' Load a document.
                 processor.LoadDocument("..\..\Document.pdf")
-
                 ' Get a list of annotations in the document pages.
                 For i As Integer = 0 To processor.Document.Pages.Count
                     Dim annotations As IList(Of PdfMarkupAnnotationData) = processor.GetMarkupAnnotationData(i)
-
                     ' Delete all text markup annotations from document pages.
                     processor.DeleteMarkupAnnotations(annotations)
-
                     ' Save the result document.
                     processor.SaveDocument("..\..\Result.pdf")
-                Next i
+                Next
             End Using
         End Sub
     End Class
